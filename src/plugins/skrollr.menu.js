ceee7473348abe7d100ff5fb729d318364d649e6
Steve Mclelland <steve@modalstudios.com>
2013-04-06 06:50:58 -0500

    Finish implementing pushState w/ popState

--- src/plugins/skrollr.menu.js
+++ src/plugins/skrollr.menu.js
@@ -10,8 +10,8 @@
   var DEFAULT_DURATION = 500;
   var DEFAULT_EASING = 'sqrt';
 
-  var TOP_OFFSET_ATTRIBUTE = 'data-menu-top';
-  var TARGET_OFFSET_ATTRIBUTE = 'data-menu-offset';
+  var MENU_TOP_ATTR    = 'data-menu-top';
+  var MENU_OFFSET_ATTR = 'data-menu-offset';
 
   /*
     Since we are using event bubbling, the element that has been clicked
@@ -32,30 +32,8 @@
     return findParentLink(element.parentNode);
   };
 
-  /*
-    Do the actual scrolling given an anchor and an offset
-  */
-  var navigate = function(offset, href) {
-    if (!offset) {
-      // Check if browser supports pushstate, if so, push the hash on
-      if (this.history && this.history.pushState) {
-        history.pushState({}, "", href);
-      }
-
-      var scrollTarget = document.getElementById(href.substr(1));
-
-      //Ignore the click if no target is found.
-      if (!scrollTarget) {
-        return;
-      }
-
-      offset = _skrollrInstance.relativeToAbsolute(scrollTarget, 'top', 'top');
-
-      // add/subtract using the scrollTarget's offset attr val
-      if (scrollTarget.hasAttribute(TARGET_OFFSET_ATTRIBUTE)) {
-        offset += +scrollTarget.getAttribute(TARGET_OFFSET_ATTRIBUTE);
-      }
-    }
+  var scroll = function(offset) {
+    _lastOffset = offset;
 
     //Now finally scroll there.
     if (_animate) {
@@ -69,6 +47,53 @@
   };
 
   /*
+    Do the actual scrolling given an anchor and an offset
+  */
+  var navigate = function(link) {
+    var offset = 0;
+    var hash = null;
+
+    if (link) {
+      //Don't use the href property because it contains the absolute url.
+      hash = link.getAttribute('href');
+
+      //Check if it's a hashlink.
+      if (hash.indexOf('#') === 0) {
+
+        if (link.hasAttribute(MENU_TOP_ATTR)) {
+          offset = +link.getAttribute(MENU_TOP_ATTR);
+          scroll(offset);
+        } else if (hash) {
+
+          var scrollTarget = document.getElementById(hash.substr(1));
+
+          //Ignore the click if no target is found.
+          if (scrollTarget) {
+
+            offset = _skrollrInstance.relativeToAbsolute(scrollTarget, 'top', 'top');
+
+            // add/subtract using the scrollTarget's offset attr val
+            if (scrollTarget.hasAttribute(MENU_OFFSET_ATTR)) {
+              offset += +scrollTarget.getAttribute(MENU_OFFSET_ATTR);
+            }
+
+            scroll(offset);
+          }
+        }
+      } else {
+        // scroll to the top
+        scroll(0);
+      }
+
+    } else {
+      // scroll to the top
+      scroll(0);
+    }
+
+    return hash;
+  };
+
+  /*
       Handle the click event on the document.
     */
   var handleClick = function(e) {
@@ -79,22 +104,37 @@
       return;
     }
 
-    //Don't use the href property because it contains the absolute url.
-    var href = link.getAttribute('href');
+    hash = navigate(link);
 
-    //Check if it's a hashlink.
-    if (href.indexOf('#') !== 0) {
-      return;
+    if (hash) {
+      // Check if browser supports pushstate + we're visiting a different hash
+      if (_hasPushState && location.hash !== hash) {
+        history.pushState({}, "", hash);
+      }
+
+      e.preventDefault();
     }
+  };
 
-    var offset = null;
-    if (link.hasAttribute(TOP_OFFSET_ATTRIBUTE)) {
-      offset = +link.getAttribute(TOP_OFFSET_ATTRIBUTE);
+  /*
+    Handle the popstate event
+  */
+  var handlePopState = function(e) {
+    if (location.hash.length > 1) {
+      // override browser's auto scroll by resetting to current position
+      window.scrollTo(0, _lastOffset);
+
+      var link = document.querySelector('a[href^="' + location.hash + '"]');
+
+      if (link) {
+        navigate(link);
+      }
+    } else if (_lastOffset > 0) {
+      // override browser's auto scroll by resetting to current position
+      window.scrollTo(0, _lastOffset);
+
+      navigate(null);
     }
-
-    navigate(offset, href);
-
-    e.preventDefault();
   };
 
   /*
@@ -111,6 +151,7 @@
 
     //Use event bubbling and attach a single listener to the document.
     skrollr.addEvent(document, 'click', handleClick);
+    skrollr.addEvent(window, 'popstate', handlePopState);
   };
 
   //Private reference to the initialized skrollr.
@@ -119,15 +160,14 @@
   var _easing;
   var _duration;
   var _animate;
+  var _hasPushState = !! (this.history && this.history.pushState);
+  var _lastOffset = 0;
 
-  //In case the page was opened with a hash, prevent jumping to it.
-  //http://stackoverflow.com/questions/3659072/jquery-disable-anchor-jump-when-loading-a-page
+  // In case the page was opened with a hash, prevent jumping to it.
+  // http://stackoverflow.com/questions/3659072/jquery-disable-anchor-jump-when-loading-a-page
   window.setTimeout(function() {
     if (location.hash) {
       window.scrollTo(0, 0);
-
-      // navigate properly
-      navigate(null, location.hash);
     }
   }, 1);
 
