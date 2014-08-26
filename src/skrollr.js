f87e2179a7f4883945510d6d560ad1c2edce970f
Daniel Johnson <djohnson@squarespace.com>
2014-08-26 15:42:03 -0400

    Adding per-element precision via data-precision

--- src/skrollr.js
+++ src/skrollr.js
@@ -358,6 +358,9 @@
 			//If this particular element should emit keyframe events.
 			var emitEvents = false;
 
+			//The interpolation precision for this particular element.
+			var precision = _precision;
+
 			//If we're reseting the counter, remove any old element ids that may be hanging around.
 			if(ignoreID && SKROLLABLE_ID_DOM_PROPERTY in el) {
 				delete el[SKROLLABLE_ID_DOM_PROPERTY];
@@ -401,6 +404,13 @@
 				//Is this element tagged with the `data-emit-events` attribute?
 				if(attr.name === 'data-emit-events') {
 					emitEvents = true;
+
+					continue;
+				}
+
+				//Global precision can be overwritten by the element attribute.
+				if(attr.name === 'data-precision') {
+					precision = parseInt(attr.value, 10);
 
 					continue;
 				}
@@ -495,6 +505,7 @@
 				smoothScrolling: smoothScrollThis,
 				edgeStrategy: edgeStrategy,
 				emitEvents: emitEvents,
+				precision: precision,
 				lastFrameIndex: -1
 			};
 
@@ -1022,7 +1033,7 @@
 							progress = left.props[key].easing(progress);
 
 							//Interpolate between the two values
-							value = _calcInterpolation(left.props[key].value, right.props[key].value, progress);
+							value = _calcInterpolation(left.props[key].value, right.props[key].value, progress, skrollable.precision);
 
 							value = _interpolateString(value);
 
@@ -1300,7 +1311,7 @@
 	/**
 	 * Calculates the new values for two given values array.
 	 */
-	var _calcInterpolation = function(val1, val2, progress) {
+	var _calcInterpolation = function(val1, val2, progress, precision) {
 		var valueIndex;
 		var val1Length = val1.length;
 
@@ -1316,7 +1327,7 @@
 
 		for(; valueIndex < val1Length; valueIndex++) {
 			//That's the line where the two numbers are actually interpolated.
-			interpolated[valueIndex] = (val1[valueIndex] + ((val2[valueIndex] - val1[valueIndex]) * progress)).toFixed(_precision);
+			interpolated[valueIndex] = (val1[valueIndex] + ((val2[valueIndex] - val1[valueIndex]) * progress)).toFixed(precision);
 		}
 
 		return interpolated;
